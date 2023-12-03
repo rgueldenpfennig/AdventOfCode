b6946beeb8ffbed5c54ae6f717e1ca1dffa2a082
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2023-12-03 23:37:04 +0100

    add unit test, resolve second half of day 1

---
+++ src/AdventOfCode.Tests/2023/D01/Day01Test.cs
@@ -0,0 +1,27 @@
+﻿using AdventOfCode.Y2023.D01;
+
+namespace AdventOfCode.Tests.Y2023.D01;
+
+public class Day01Test
+{
+    [Theory]
+    [InlineData("51591twosix4dhsxvgghxq", 54)]
+    [InlineData("425nine", 49)]
+    [InlineData("5sjnnfivefourzxxfpfivenine7five", 55)]
+    [InlineData("7hjmmxhdnine8", 78)]
+    [InlineData("26seven1", 21)]
+    [InlineData("two1nine", 29)]
+    [InlineData("eightwothree", 83)]
+    [InlineData("abcone2threexyz", 13)]
+    [InlineData("xtwone3four", 24)]
+    [InlineData("4nineeightseven2", 42)]
+    [InlineData("zoneight234", 14)]
+    [InlineData("7pqrstsixteen", 76)]
+    public void CalculateResult(string input, int expectedResult)
+    {
+        var sut = new Day01();
+        var result = sut.CalculateResult(input);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
---
+++ src/AdventOfCode/2023/D01/Day01.cs
@@ -0,0 +1,75 @@
+﻿namespace AdventOfCode.Y2023.D01;
+
+internal class Day01 : Problem
+{
+    public override async ValueTask SolveAsync()
+    {
+        var calibrationValues = await File.ReadAllLinesAsync(Path.Combine(Environment.CurrentDirectory, "2023", "D01", "input.txt"));
+        var calculations = new List<int>(capacity: calibrationValues.Length);
+
+        foreach (var value in calibrationValues)
+        {
+            var calculation = CalculateResult(value);
+            calculations.Add(calculation);
+        }
+
+        Console.WriteLine($"Result: {calculations.Sum()}");
+    }
+
+    private readonly Dictionary<int, string> _letters = new()
+    {
+        { 1, "one" },
+        { 2, "two" },
+        { 3, "three" },
+        { 4, "four" },
+        { 5, "five" },
+        { 6, "six" },
+        { 7, "seven" },
+        { 8, "eight" },
+        { 9, "nine" }
+    };
+
+    internal int CalculateResult(string input)
+    {
+        var span = input.AsSpan();
+        int firstDigit = 0;
+        int lastDigit = 0;
+
+        bool firstDigitSet = false;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            var character = input[i];
+            if (char.IsDigit(character))
+            {
+                if (!firstDigitSet)
+                {
+                    firstDigit = (int)char.GetNumericValue(character);
+                    firstDigitSet = true;
+                }
+
+                lastDigit = (int)char.GetNumericValue(character);
+            }
+            else
+            {
+                var range = span[i..];
+                foreach (var letterPair in _letters)
+                {
+                    if (range.StartsWith(letterPair.Value, StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (!firstDigitSet)
+                        {
+                            firstDigit = letterPair.Key;
+                            firstDigitSet = true;
+                        }
+
+                        lastDigit = letterPair.Key;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return 10 * firstDigit + lastDigit;
+    }
+}
