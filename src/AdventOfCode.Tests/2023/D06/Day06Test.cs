24791dbe805aea39194abb9c949feaf607caa117
rgueldenpfennig <mail@robin-gueldenpfennig.de>
2023-12-06 18:32:53 +0100

    solve example of day 6

---
+++ src/AdventOfCode.Tests/2023/D06/Day06Test.cs
@@ -0,0 +1,33 @@
+﻿using AdventOfCode.Y2023.D06;
+using static AdventOfCode.Y2023.D06.Day06;
+
+namespace AdventOfCode.Tests.Y2023.D06;
+
+public class Day06Test
+{
+    [Fact]
+    public void ShouldSolveExample()
+    {
+        var sut = new Day06();
+        var result = sut.SolveExample();
+
+        Assert.Equal(288U, result);
+    }
+
+    [Theory]
+    [InlineData(7, 9, 0, 0)]
+    [InlineData(7, 9, 1, 6)]
+    [InlineData(7, 9, 2, 10)]
+    [InlineData(7, 9, 3, 12)]
+    [InlineData(7, 9, 4, 12)]
+    [InlineData(7, 9, 5, 10)]
+    [InlineData(7, 9, 6, 6)]
+    [InlineData(7, 9, 7, 0)]
+    public void ShouldHandleGetDistanceByCharge(uint time, uint distance, uint chargeTime, uint expectedDistance)
+    {
+        var sut = new Race(time, distance);
+        var result = sut.GetDistanceByCharge(chargeTime);
+
+        Assert.Equal(expectedDistance, result);
+    }
+}
---
+++ src/AdventOfCode/2023/D06/Day06.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode.Y2023.D06;
+
+internal class Day06 : Problem<uint>
+{
+    internal record struct Race(uint Time, uint Distance)
+    {
+        public uint GetDistanceByCharge(uint chargeTime)
+        {
+            if (chargeTime == 0 || chargeTime >= Time) return 0;
+
+            var diff = Time - chargeTime;
+            return diff * chargeTime;
+        }
+    }
+
+    public uint SolveExample()
+    {
+        var races = new[] { new Race(7, 9), new Race(15, 40), new Race(30, 200) };
+        var possibilities = new List<uint>(capacity: races.Length);
+
+        foreach (var race in races)
+        {
+            uint minimumChargeTime;
+            uint maximumChargeTime;
+            for (uint i = 1; ; i++)
+            {
+                var distance = race.GetDistanceByCharge(i);
+                if (distance > race.Distance)
+                {
+                    minimumChargeTime = i;
+                    break;
+                }
+            }
+
+            for (uint i = race.Time - 1; ; i--)
+            {
+                var distance = race.GetDistanceByCharge(i);
+                if (distance > race.Distance)
+                {
+                    maximumChargeTime = i;
+                    break;
+                }
+            }
+
+            possibilities.Add(1 + maximumChargeTime - minimumChargeTime);
+        }
+
+        return possibilities.Aggregate(1U, (x, y) => x * y);
+    }
+}
